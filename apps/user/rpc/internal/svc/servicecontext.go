47d6ce09273e21718da5e301351ac24654c1ac63
mingri31164 <3116430062@qq.com>
2025-10-10 00:29:57 +0800

    ✨feat: 初始构建user-rpc

---
+++ apps/user/rpc/internal/svc/servicecontext.go
@@ -0,0 +1,13 @@
+package svc
+
+import "SAI-IM/apps/user/rpc/internal/config"
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}

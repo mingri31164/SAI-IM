78ebae2c4f487f76c79ee3db44f4e88533981109
mingri31164 <3116430062@qq.com>
2025-10-23 04:04:46 +0800

    ✨feat: 初始构建user-api

---
+++ apps/user/api/internal/svc/servicecontext.go
@@ -0,0 +1,19 @@
+package svc
+
+import (
+	"SAI-IM/apps/user/api/internal/config"
+	"SAI-IM/apps/user/rpc/userclient"
+	"github.com/zeromicro/go-zero/zrpc"
+)
+
+type ServiceContext struct {
+	Config config.Config
+	userclient.User
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+		User:   userclient.NewUser(zrpc.MustNewClient(c.UserRpc)),
+	}
+}

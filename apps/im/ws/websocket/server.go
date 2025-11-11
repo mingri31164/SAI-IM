fffffc781e5c36dd24e1588eb98c81eb895600fc
mingri31164 <3116430062@qq.com>
2025-11-11 21:59:15 +0800

    ✨feat: 完成路由定义

--- apps/im/ws/websocket/server.go
+++ apps/im/ws/websocket/server.go
@@ -2,6 +2,7 @@ package websocket
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/websocket"
 	"github.com/zeromicro/go-zero/core/logx"
@@ -9,6 +10,7 @@ import (
 )
 
 type Server struct {
+	routes   map[string]HandlerFunc
 	addr     string
 	upgrader websocket.Upgrader
 	logx.Logger
@@ -16,6 +18,7 @@ type Server struct {
 
 func NewServer(addr string) *Server {
 	return &Server{
+		routes:   make(map[string]HandlerFunc),
 		addr:     addr,
 		upgrader: websocket.Upgrader{},
 		Logger:   logx.WithContext(context.Background()),
@@ -31,14 +34,50 @@ func (s *Server) ServerWs(w http.ResponseWriter, r *http.Request) {
 	}()
 
 	// 获取连接对象
-	_, err := s.upgrader.Upgrade(w, r, nil)
+	conn, err := s.upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		s.Errorf("upgrade err %v", err)
 		return
 	}
 
-	// 根据连接对象获取请求
+	// 根据连接对象获取请求，根据请求查找路由并执行
+	go s.handlerConn(conn)
 
+}
+
+// 根据连接对象执行任务处理
+func (s *Server) handlerConn(conn *websocket.Conn) {
+
+	for {
+		// 获取请求消息
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			s.Errorf("websocket conn read message err %v", err)
+			// TODO: 关闭连接
+			return
+		}
+		// 解析消息
+		var message Message
+		if err = json.Unmarshal(msg, &message); err != nil {
+			s.Errorf("json unmarshal err %v, msg %v", err, string(msg))
+			// TODO: 关闭连接
+			return
+		}
+
+		// 根据请求的method分发路由并执行
+		if handler, ok := s.routes[message.Method]; ok {
+			handler(s, conn, &message)
+		} else {
+			conn.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf("不存在执行的方法 %v，请检查",
+				message.Method)))
+		}
+	}
+}
+
+func (s *Server) AddRoutes(rs []Route) {
+	for _, r := range rs {
+		s.routes[r.Method] = r.Handler
+	}
 }
 
 func (s *Server) Start() {

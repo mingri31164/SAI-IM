7f65f244ef56f8b7f57c177761a8b1e2de3109c1
mingri31164 <3116430062@qq.com>
2025-11-18 02:22:42 +0800

    ✨feat: 完成会话更新

--- apps/im/rpc/internal/logic/putconversationslogic.go
+++ apps/im/rpc/internal/logic/putconversationslogic.go
@@ -1,7 +1,11 @@
 package logic
 
 import (
+	"SAI-IM/apps/im/immodels"
+	"SAI-IM/pkg/constants"
+	"SAI-IM/pkg/xerr"
 	"context"
+	"github.com/pkg/errors"
 
 	"SAI-IM/apps/im/rpc/im"
 	"SAI-IM/apps/im/rpc/internal/svc"
@@ -25,7 +29,32 @@ func NewPutConversationsLogic(ctx context.Context, svcCtx *svc.ServiceContext) *
 
 // 更新会话
 func (l *PutConversationsLogic) PutConversations(in *im.PutConversationsReq) (*im.PutConversationsResp, error) {
-	// todo: add your logic here and delete this line
-
+	data, err := l.svcCtx.ConversationsModel.FindByUserId(l.ctx, in.UserId)
+	if err != nil {
+		return nil, errors.Wrapf(xerr.NewDBErr(), "conversationModel.FindByUserId err %v,req %v", err, in.UserId)
+	}
+	// 存在会话，但是会话列表可能为空，需要初始化
+	if data.ConversationList == nil {
+		data.ConversationList = make(map[string]*immodels.Conversation)
+	}
+	for s, conversation := range in.ConversationList {
+		var oldTotal int
+		if data.ConversationList[s] != nil {
+			// 用户在当前会话中在此之前已读取的量
+			oldTotal = data.ConversationList[s].Total
+		}
+		data.ConversationList[s] = &immodels.Conversation{
+			ConversationId: conversation.ConversationId,
+			ChatType:       constants.ChatType(conversation.ChatType),
+			IsShow:         conversation.IsShow,
+			// ✨用户传递的已读取的量+原本已读取的量 = 实际用户已读取量
+			Total: int(conversation.Read) + oldTotal,
+			Seq:   conversation.Seq,
+		}
+	}
+	_, err = l.svcCtx.ConversationsModel.Update(l.ctx, data)
+	if err != nil {
+		return nil, errors.Wrapf(xerr.NewDBErr(), "conversationModel.Update err %v,req %v", err, data)
+	}
 	return &im.PutConversationsResp{}, nil
 }

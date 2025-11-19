d0e5cbd1259337a1f398e88e9bc6e15d7a575ed3
mingri31164 <3116430062@qq.com>
2025-11-20 05:49:12 +0800

    ✨feat: 用户登录状态缓存

--- apps/user/api/internal/svc/servicecontext.go
+++ apps/user/api/internal/svc/servicecontext.go
@@ -3,20 +3,21 @@ package svc
 import (
 	"SAI-IM/apps/user/api/internal/config"
 	"SAI-IM/apps/user/rpc/userclient"
+	"github.com/zeromicro/go-zero/core/stores/redis"
 	"github.com/zeromicro/go-zero/zrpc"
 	// N * client =》 别名
 )
 
 type ServiceContext struct {
 	Config config.Config
-
 	userclient.User
+	*redis.Redis
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
 		Config: c,
-
-		User: userclient.NewUser(zrpc.MustNewClient(c.UserRpc)),
+		User:   userclient.NewUser(zrpc.MustNewClient(c.UserRpc)),
+		Redis:  redis.MustNewRedis(c.Redisx),
 	}
 }

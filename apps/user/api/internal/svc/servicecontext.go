bbdcd0456338c84c874747457ce591e383e16cae
mingri31164 <3116430062@qq.com>
2025-11-23 22:42:53 +0800

    🧪test: grpc重试机制测试

--- apps/user/api/internal/svc/servicecontext.go
+++ apps/user/api/internal/svc/servicecontext.go
@@ -8,6 +8,24 @@ import (
 	// N * client =》 别名
 )
 
+// google.golang.org\grpc\server_config.go -> jsonMc jsonRetryPolicy
+// grpc重试机制配置文件
+//var retryPolicy = `{
+//	"methodConfig" : [{
+//		"name": [{
+//			"service": "user.User"
+//		}]
+//		"waitForReady": true,
+//		"retryPolicy": {
+//			"maxAttempts": 5,
+//			"initialBackoff": "0.001s",
+//			"maxBackoff": "0.002s",
+//			"backoffMultiplier": 1.0,
+//			"retryableStatusCodes": ["UNKNOWN"]
+//		}
+//	}]
+//}`
+
 type ServiceContext struct {
 	Config config.Config
 	userclient.User
@@ -19,5 +37,9 @@ func NewServiceContext(c config.Config) *ServiceContext {
 		Config: c,
 		User:   userclient.NewUser(zrpc.MustNewClient(c.UserRpc)),
 		Redis:  redis.MustNewRedis(c.Redisx),
+
+		// 实践grpc重试机制
+		//User: userclient.NewUser(zrpc.MustNewClient(c.UserRpc,
+		//	zrpc.WithDialOption(grpc.WithDefaultServiceConfig(retryPolicy)))),
 	}
 }
